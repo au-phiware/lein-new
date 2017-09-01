a97f7e2ca8136c15a448d041fe417bb1c7d23270
Corin Lawson <corin@responsight.com>
2017-09-02 00:08:19 +1000

    Ensure *main-cli-fn* is actually a function.

--- env/dev/server/lein_new/start.cljs
+++ env/dev/server/lein_new/start.cljs
@@ -7,7 +7,7 @@
 
 (.on js/process "uncaughtException" #(js/console.error %))
 
-(set! *main-cli-fn* (-> (mount/swap
+(set! *main-cli-fn* #(-> (mount/swap
                           {#'lein-new.env/env {:document-root "target/dev/client"}})
                         mount/start))
 
--- env/prod/server/lein_new/start.cljs
+++ env/prod/server/lein_new/start.cljs
@@ -7,7 +7,7 @@
 
 (let [doc-root (.resolve (nodejs/require "path")
                          js/__dirname ".." ".." "client")]
-  (set! *main-cli-fn* (-> (mount/swap
+  (set! *main-cli-fn* #(-> (mount/swap
                             {#'lein-new.env/env
                              {:document-root doc-root}})
                           mount/start)))
